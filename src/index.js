364453d57d27b1bf8075006c94f9250b1dfe7be7
Pavel Voitihovich <pasha2003228@gmail.com>
2021-08-17 18:21:48 +0300

    feat: compleate the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    let arrayNum = [...num.toString()].map(Number);
+    //console.log(arrayNum.length);
+    if (arrayNum.length === 4) {
+        arrayNum = arrayNum.slice(1);
+    }
+    return arrayNum.reverse().join('');
 }
